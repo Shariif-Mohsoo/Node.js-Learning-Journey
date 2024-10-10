74b9eb8ed90404b6c541d9c88bd75387984c882b
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-09 21:04:19 -0700

    .\expressHandlingPostRequest.js

--- expressHandlingPostRequest.js
+++ expressHandlingPostRequest.js
@@ -1,11 +1,14 @@
 import express from "express";
 import fs from "fs";
+// importing the middleware
+import middlewares from "./middlewares.js";
+const { validateBody } = middlewares;
 const app = express();
 app.use(express.json());
 const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
-app.post("/api/v1/movies", (req, res) => {
+app.post("/api/v1/movies", validateBody, (req, res) => {
   const newId = movies[movies.length - 1].id + 1;
-  console.log(newId);
+  // console.log(newId);
   const newMovie = Object.assign(req.body, { id: newId });
   //writing back data
   fs.writeFile("./data/movies.json", JSON.stringify(movies, null, 2), (err) => {
