d86c137408984f41d6c3aa607a830141f621de92
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-11 21:33:51 -0700

    PUBLIC / STATIC FILES

---
+++ public/templates/index.js
@@ -0,0 +1,24 @@
+export default function () {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <title>SERVING THE STATIC FILES</title>
+  </head>
+  <body>
+  <div class="container">
+      <h1>HELLO MOHSOO</h1>
+      <p>
+        WELCOME TO THE DEMO OF HOW TO SERVE STATIC
+        FILES IN EXPRESS JS?
+        THIS IS THE SIMPLE DEMO HTML PAGE WITH LITTLE
+        BIT CSS.
+      </p>
+  </div>
+  </body>
+</html>
+`;
+}
